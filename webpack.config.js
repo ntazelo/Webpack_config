90bca66f5af4309a5bfc0ad43ec021051857842e
ntazelo <ntazelo@gmail.com>
2021-10-27 16:49:36 +0200

    Fix linters errors#3

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/page.html',
+      template: './src/index.html',
     }),
   ],
   devServer: {
